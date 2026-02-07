42b1e73a8b3e4d7eaf2e5f8e01df78881c7a7a64
PUTIJIRAF\llucs <llucsegala29@gmail.com>
2026-02-07 14:28:58 +0100

    improve user flow by validating calendar existence before event creation

--- gestor_calendaris.py
+++ gestor_calendaris.py
@@ -25,9 +25,9 @@
 """
 
 
-
 import sqlite3
 import os
+
 
 # --- CONFIGURACIÓ INICIAL ---
 carpeta_actual = os.path.dirname(os.path.abspath(__file__))
@@ -35,21 +35,23 @@ ruta_db = os.path.join(carpeta_actual, 'meu_calendari.db')
 connexio = sqlite3.connect(ruta_db)
 cursor = connexio.cursor()
 
+
+
 def mostrar_menu():
-    print("\n---  GESTOR DE CALENDARIS ---")
+    print("\n\n---  GESTOR DE CALENDARIS ---")
     print("1. Veure tots els calendaris")
     print("2. Crear un nou calendari")
     print("3. Menu esdeveniments")
     print("4. Sortir")
-    return input("Tria una opció: ")
-
+    return input("\nTria una opció: ")
 
 def mostrar_menu_esdeveniments():
     print("\nOpcions:")
     print("1. Veure esdeveniments d'un calendari")
     print("2. Crear nou esdeveniment")
-    print("3. Tornar al menú principal")
+    print("3. Tornar al menú principal\n")
     return input("Tria una opció: ")
+
 
 
 while True:
@@ -57,68 +59,102 @@ while True:
 
     if opcio == "1":
         # LLISTAR
-        
         cursor.execute("SELECT * FROM calendaris")
         files = cursor.fetchall()
         
         if not files:
-            print("No tens cap calendari creat encara.")
+            print("\nNo tens cap calendari creat encara.")
+            input("\nEnter per continuar")
         
         else:
             print("\nELS TEUS CALENDARIS:")
             for f in files:
                 print(f"[{f[0]}] - {f[1]}")
-            
-        
+            input("\nEnter per continuar")         
     
     
     elif opcio == "2":
         # AFEGIR
-        nom = input("Nom del nou calendari: ")
+        nom = input("\nNom del nou calendari: ")
         cursor.execute("INSERT INTO calendaris (nom) VALUES (?)", (nom,))
         connexio.commit()
-        print(f"'{nom}' guardat!")
+        print(f"\n'{nom}' guardat!")
+        input("\nEnter per continuar")
 
-    
     
     elif opcio == "3":
         #Menu esdeveniments
         while True:
-            
             opcio_calendari = mostrar_menu_esdeveniments()
             
             if opcio_calendari == "1":
-                    id_calendari = input("\nID del calendari que vols veure: ")
-                    cursor.execute("SELECT * FROM esdeveniments WHERE calendari_id = (?)", 
-                                (id_calendari,)
-                    )
+                   
+                    # Primer mirem si el usuari te calendaris on puguin haver esdeveniments
+                    cursor.execute("SELECT calendari_id FROM esdeveniments")
+                    Hi_ha_esdeveniments = cursor.fetchall()
                     
-                    esdeveniments = cursor.fetchall()
-                    for e in esdeveniments:
-                        print(f"\n[{e[0]}]- {e[1]} el {e[2]} a les {e[3]}")
+                    if not Hi_ha_esdeveniments:
+                        print("\nEncara no tens cap calendari o esdeveniments per veurels")
+                        input("\nEnter per continuar")
                     
-            if opcio_calendari == "2":
-                    id_calendari = input("Numero del calendari on vols afegir l'esdeveniment: ")
-                    nom_esdeveniment = input("Nom de l'esdeveniment: ")
-                    data_esdeveniment = input("Data: ")
-                    hora_esdeveniment = input("Hora: ")
+                    # Aqui mostrem els esdeveniments del calendari que vulgui
+                    else:
+                        id_calendari = input("\nID del calendari que vols veure: ")
                     
-                    cursor.execute(
-                        "INSERT INTO esdeveniments (nom, data, hora, calendari_id) VALUES (?, ?, ?, ?)", 
-                        (nom_esdeveniment, data_esdeveniment, hora_esdeveniment, id_calendari)
-                    )
-                    connexio.commit()
-                    print(f"Esdeveniment '{nom_esdeveniment}' afegit al calendari ID {id_calendari}!")
+                        cursor.execute("SELECT * FROM esdeveniments WHERE calendari_id = (?)", 
+                                    (id_calendari,)
+                        )
+                        
+                        esdeveniments = cursor.fetchall()
+                        
+                        for e in esdeveniments:
+                            print(f"\n[{e[0]}]- {e[1]} el {e[2]} a les {e[3]}")
+                            input("\nEnter per continuar")
+                    
+            elif opcio_calendari == "2":
+                
+                cursor.execute("SELECT id FROM calendaris")
+                Hi_ha_calendari = cursor.fetchall()
+                    
+                if not Hi_ha_calendari:
+                    print("\nEncara no tens cap calendari on puguis crear els esdeveniments, primer crea un calendari i despres podras crear un esdeveniments")
+                    input("\nEnter per continuar")
+                    
+                else:
+                    
+                    while True:
+                        id_calendari = input("Numero del calendari on vols afegir l'esdeveniment: ")
+                        
+                        cursor.execute("SELECT id FROM calendaris WHERE id = ?", (id_calendari))
+                        Existeix_id = cursor.fetchone()
+                        
+                        if Existeix_id == None:
+                            
+                            print("Aquet calendari no existeix")
+                            input("\nEnter per continuar")
+                            
+                        else:                    
+                            nom_esdeveniment = input("Nom de l'esdeveniment: ")
+                            data_esdeveniment = input("Data: ")
+                            hora_esdeveniment = input("Hora: ")
+                                
+                            cursor.execute(
+                                "INSERT INTO esdeveniments (nom, data, hora, calendari_id) VALUES (?, ?, ?, ?)", 
+                                (nom_esdeveniment, data_esdeveniment, hora_esdeveniment, id_calendari)
+                                )
+                            connexio.commit()
+                            print(f"Esdeveniment '{nom_esdeveniment}' afegit al calendari ID {id_calendari}!")
+                            input("\nEnter per continuar")
+                            break
                     
             if opcio_calendari == "3":
                     break
-        
-        
-    
+         
     elif opcio == "4":
-        print("Adéu!")
+        print("\nAdéu!")
         connexio.close()
         break 
 
     else:
-        print("Opció no vàlida, torna-ho a provar.")      
+        print("\nOpció no vàlida, torna-ho a provar.")      
+        input("\nEnter per continuar")
